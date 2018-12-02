32fa4d27b53ed06f0c736c2fa51c445c0a364efd
kimseungchan <withksc@gmail.com>
2018-12-02 02:43:43 -0500

    Added Chart, Database and Individualstock page

---
+++ js/database.js
@@ -0,0 +1,26 @@
+createPosition = function (position) {
+  var positionObject = {
+    //"id": ,
+    "company": position.company,
+    "price": position.price,
+    "quantity": position.quantity,
+    "date": position.date,
+    "type": position.type
+  }
+  // TODO: Unique ID
+  var id = "";
+
+
+}
+
+readPosition = function() {
+
+}
+
+updatePosition = function() {
+
+}
+
+deletePosition = function() {
+
+}
--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
     $('.marquee').marquee();
+
+
+    $("#myForm").submit(function(){
+        var orderPositions = $(this).serializeArray();
+        alert(orderPositions.toSource());  // debug
+
+        orderPositions.forEach(function(position){
+            localStorage.setItem(position.name, position.value);
+        });
+
+        alert(localStorage.getItem("company"));
+        alert(localStorage.getItem("price"));
+    });
+
+
+    // // canvas responsive
+    // resize();
+    // $(window).on("resize", function(){                      
+    //     resize();
+    // });
 });
+
+// function resize(){    
+//     $("#canvas").outerHeight($(window).height()-$("#canvas").offset().top- Math.abs($("#canvas").outerHeight(true) - $("#canvas").outerHeight()));
+// }
